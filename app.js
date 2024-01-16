9c8c1797bdac12130e99b5602e94c3924ea49f14
Natalia <nlopezsalas@gmail.com>
2024-01-16 20:07:59 +0000

    Add the domain hacks

--- app.js
+++ app.js
@@ -20,8 +20,45 @@ console.log("-----------------------------------------------");
 for (let i=0; i < pronoun.length; i++) {
     for (let j=0; j < adj.length; j++) {
         for (let k=0; k < noun.length; k++) {
-            for (let n=0; n < domain.length; n++ )
-            console.log(pronoun[i]+adj[j]+noun[k]+domain[n]);
+            for (let l=0; l < domain.length; l++ )
+            console.log(pronoun[i]+adj[j]+noun[k]+domain[l]);
         }
     }
 }
+
+console.log ("");
+console.log("DOMAIN HACKS");
+console.log("-----------------------------------------------");
+
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        let domainCrack = [];
+        let domainStart = "";
+        for (let i = 0; i < noun.length; i++) {
+            for (let j = 0; j < noun[i].length - 2; j++) {
+                domainStart += noun[i][j];
+            }
+            domainCrack[i] = domainStart;
+            domainStart = ""
+        }
+        let domainHack = [];
+        let domainEnd = "";
+        for (let i = 0; i < noun.length; i++) {
+            for (let j = noun[i].length - 2; j < noun[i].length; j++) {
+                domainEnd += noun[i][j];
+            }
+            domainHack[i] = domainEnd;
+            domainEnd = ""
+        }
+        for (let k = 0; k < noun.length; k++) {
+            console.log(pronoun[i] + adj[j] + domainCrack[k] + '.' + domainHack[k]);
+        }
+    }
+}
+
+
+
+
+
+
+
